5f81e547936a3721f34c5947f486874d745cded0
Adrian Gonzalez Vega <127346252+adrigongv23@users.noreply.github.com>
2026-08-03 21:35:15 +0200

    Delete Plataform_Web directory

--- Plataform_Web/temporadas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TemporadasConfig(AppConfig):
-    name = 'temporadas'
--- Plataform_Web/users/decorators.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.decorators import login_required, user_passes_test
-
-
-def require_rol(*roles):
-    def decorator(view_func):
-        return login_required(user_passes_test(lambda u: u.rol in roles)(view_func))
-    return decorator
